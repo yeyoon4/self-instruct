d3fbace58025b8f8f512a3bd88383055a0b8a18a
yeyoon4 <56835254+yeyoon4@users.noreply.github.com>
2023-07-19 15:48:02 +0900

    Update generate_instructions.sh

--- scripts/generate_instructions.sh
+++ scripts/generate_instructions.sh
@@ -2,6 +2,6 @@ batch_dir=data/gpt3_generations/
 
 python self_instruct/bootstrap_instructions.py \
     --batch_dir ${batch_dir} \
-    --num_instructions_to_generate 50000 \
+    --num_instructions_to_generate 10 \
     --seed_tasks_path data/seed_tasks.jsonl \
     --engine "davinci"
